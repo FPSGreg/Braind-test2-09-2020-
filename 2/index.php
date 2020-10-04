21ca908fb2af50e1abc2ca94fac6f27735827649
FPSGreg <48761431+FPSGreg@users.noreply.github.com>
2020-10-04 14:32:13 +0300

    Поправки в тестовую
    Внесение поправок после обратной связи.

--- 2/index.php
+++ 2/index.php
@@ -2,28 +2,35 @@
 
 
 if (isset($_POST["quantity"])){
-    for($i=1;$i<=$_POST["quantity"];$i++){
+    for($i=0;$i<=$_POST["quantity"];$i++){
     $array[$i]=$i;
-    }
+    };
 
 //    sort($array, SORT_STRING );
 
-for ($j = 0; $j < count($array) - 1; $j++){
+for ($j = 0; $j < count($array) + 1; $j++){
     for ($i = 0; $i < count($array) - $j - 1; $i++){
-            if (strcmp((string)$array[$i],(string)$array[$i + 1]) == 1){
+            if (strcmp($array[$i],$array[$i + 1]) == 1){
             $tmp_var = $array[$i + 1];
             $array[$i + 1] = $array[$i];
             $array[$i] = $tmp_var;
-        }
-    }
-}
+            var_dump($array);
+            echo"<br>";
+        };
+    };
+};
 
-    $result = (array_search($_POST["number"], $array))+1;
+
+
+
+ var_dump($array);
+
+    $result = (array_search($_POST["number"], $array));
 
     echo ("Позиция числа {$_POST["number"]} - $result ая");
 }else{
     echo "Введите число";
-}
+};
 
 ?>
 
